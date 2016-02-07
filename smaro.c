7f99bcf2e6c30565db4e0ea9bc573ddf1e36bdfc
lennarto <lennarto@users.noreply.github.com>
2016-02-08 00:00:26 +0100

    Sabrina, Franz, Lennart

--- smaro.c
+++ smaro.c
@@ -5,11 +5,21 @@
 // SPEED DEFINIEREN
 int slopespeed = 45;
 int slopespeed_fast = 55;
-int slopespeed_slow = 45;
+int slopespeed_slow = 30;
+int wenden_speed = 45;
+int delay_n = 10000000/3;
+
+// TURN AROUND DELAY
+int delayXXX;
+int delay_kurz = 10000000*0.60;
+int delay_lang = 10000000*0.75;
+
+// LIGHTS
+volatile unsigned int *pLights = (volatile unsigned int*)(0x80000000 | LIGHTING_0_BASE);
 
 // LOGIK FWD BWD
-// V = 1  : VORWÄRTS
-// V = -1 : RÜCKWÄRTS
+// V = 1  : VORW�RTS
+// V = -1 : R�CKW�RTS
 // r = -1 : FLAG : noch nicht gewendet
 int v = 1;
 int r = -1;
@@ -17,14 +27,14 @@ int r = -1;
 int main() {
 	// VARIABLEN INITIALISIEREN
 	printf("START\n!!!!!!!!!!!!!!!!!!!!!!!!\n");
+	int a = 0;
 	init();
-	short AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ, x, y, z;
+	short AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
 	char cBuff[32];
-	
+	int wenden_speed;
+
 	// LICHT
-	//volatile unsigned int *pLights = (volatile unsigned int*)(0x80000000 | LIGHTING_0_BASE);
-	//*pLights = 0xf;
-	
+	*pLights = 0xf;
 	*pwm_enable = 0;
 
 	I2CWrite(MPU_SLAVE_ADDRESS, 0x6b, 0x0);
@@ -32,7 +42,7 @@ int main() {
 
 	while(1)
 	{
-		// TODO Wir brauchen nur AcX und AcY
+		// Wir brauchen nur AcX und AcY
 		AcX = AcY = AcZ = Tmp = GyX = GyY = GyZ = 0;
 		I2CRead(MPU_SLAVE_ADDRESS, 0x3B, 14, cBuff);
 		AcX = (cBuff[0] << 8) | (cBuff[1] & 0xff);
@@ -43,16 +53,16 @@ int main() {
 		GyY = (cBuff[10] << 8) | (cBuff[11] & 0xff);
 		GyZ = (cBuff[12] << 8) | (cBuff[13] & 0xff);
 
-		printf("AcX = %d\n", AcX);
-		printf("AcY = %d/n", AcY);
+		printf("AcX = %d - ", AcX);
+		printf("AcY = %d\n", AcY);
 
-		// WERTE AUF STANDARD SETZTEN 
+		// WERTE AUF STANDARD SETZTEN
 		// UND AUTO GERADEAUS FAHREN LASSEN
 		set_duty_cycle(pFrontRightDutySet, slopespeed);
 		set_duty_cycle(pRearRightDutySet, slopespeed);
 		set_duty_cycle(pRearLeftDutySet, slopespeed);
 		set_duty_cycle(pFrontLeftDutySet, slopespeed);
-		
+
 		// LOGIK FWD BWD
 		if(v > 0){
 			slow(ALL_WHEEL_FWD_MASK);
@@ -60,40 +70,108 @@ int main() {
 			slow(ALL_WHEEL_BWD_MASK);
 		}
 
-		// ERHÖHTE STEIGUNG
-		// RÜCKWÄRTS HOCH FAHREN
-		if (AcY < -3500*v && r < 0) {
-			wenden();
-			v = -1;
+		printf("v = %d\n", v);
+
+		// TURN AROUND
+		if (AcY < -3600*v && r < 0 && AcX > -800 && AcX < 800) {
+			*pLights = 0xf;
+			delay(1000000);
+
+			if (AcX > 200 && AcX < 300) {
+				delayXXX = 10000000*0.70;
+				printf("delay kurz 075");
+				a = 0;
+		  }
+
+			if (AcX > 301 && AcX < 400) {
+				delayXXX = 10000000*0.65;
+				printf("delay kurz 07");
+				a = 0;
+			}
+
+			if (AcX > 401 && AcX < 500) {
+				delayXXX = 10000000*0.55;
+				printf("delay kurz 06");
+			}
+
+			if (AcX > 501 && AcX < 600) {
+				delayXXX = 10000000*0.5;
+				printf("delay 05");
+				a = 0;
+			}
+
+			if (AcX < 200 && AcX > -200) {
+				delayXXX = 10000000*0.55;
+				printf("delay 07");
+				a = 0;
+		  }
+
+			if (AcX < -201 && AcX > -300) {
+				delayXXX = 10000000*0.75;
+				printf("delay 075");
+				a = 1;
+			}
+
+			if (AcX < -301 && AcX > -400) {
+				delayXXX = 10000000*0.8;
+				printf("delay 08");
+				a = 1;
+			}
+
+			if (AcX < -401 && AcX > -500) {
+			  delayXXX = 10000000*0.85;
+				printf("delay 058");
+				a = 1;
+			}
+
+			if (AcX < -501 && AcX > -600) {
+				delayXXX = 10000000*0.9;
+				printf("delay 099");
+		   	a = 1;
+			}
+
+			else {
+		    delayXXX = 10000000*0.60;
+		    printf("delay lang");
+		  }
+
+			// TURN AROUND WITH DALAY a
+			wenden(a);
+
+			pos_halten();
+			printf("!!! WENDEN !!!\n");
+
+			// v = -1;
 			r = 1;
-			slopespeed = slopespeed_fast;
-			slow(ALL_WHEEL_BWD_MASK);
+			slopespeed = slopespeed_slow;
+			*pLights = 0x1;
+			slow(ALL_WHEEL_FWD_MASK);
+			printf("v = %i\n", v);
 		}
 
-		// ZU HOHE STEIGUNG
-		// RUTSCHE WIEDER RUNTER FAHREN
-		if (AcY < -4000*v) {
-			*pwm_enable = 0;
-			v = 1;
-			slopespeed = slopespeed_slow;
-			slow(ALL_WHEEL_FWD_MASK);
+		if (AcY < -2000) {
+			delay_n = 1000000;
 		}
-		
+
+		delay_n = 1000000;
+
 		// AUTO ZU WEIT LINKS
-		if (AcX < -1200*v) {
-			printf("nach rechts");
+		if (AcX < -1000*v) {
+			printf("nach rechts\n");
 			nach_rechts();
+
 			if(v > 0){
 				slow(ALL_WHEEL_FWD_MASK);
 			} else {
 				slow(ALL_WHEEL_BWD_MASK);
 			}
 		}
-			
+
 		// AUTO ZU WEIT RECHTS
-		if (AcX > 800*v) {
-			printf("nach links");
+		if (AcX > 400*v) {
+			printf("nach links\n");
 			nach_links();
+
 			if(v > 0){
 				slow(ALL_WHEEL_FWD_MASK);
 			} else {
@@ -103,7 +181,7 @@ int main() {
 		
 		delay(10000000);
 	}
-	
+
 	return 0;
 }
 
@@ -111,21 +189,45 @@ void slow(RICHTUNG) {
 	*pwm_enable = 0;
 	delay(2);
 	*pwm_enable = (RICHTUNG);
-	delay(1000000);
+	delay(delay_n*2);
 	*pwm_enable = 0;
 }
 
-void wenden() {
+void wenden(int a) {
 	*pwm_enable = 0;
 	delay(2);
-	*pwm_enable = (TURN_LEFT_MASK);
-	delay(1000000);
-	*pwm_enable = (TURN_LEFT_MASK);
-	delay(1000000);
+	set_duty_cycle(pFrontRightDutySet, wenden_speed);
+	set_duty_cycle(pRearRightDutySet, wenden_speed);
+	set_duty_cycle(pRearLeftDutySet, wenden_speed);
+	set_duty_cycle(pFrontLeftDutySet, wenden_speed);
+	if(a==0) *pwm_enable = (TURN_LEFT_MASK);
+	if(a == 1) *pwm_enable = (TURN_RIGHT_MASK);
+	delay(delayXXX);
+
+	set_duty_cycle(pFrontRightDutySet, slopespeed);
+	set_duty_cycle(pRearRightDutySet, slopespeed);
+	set_duty_cycle(pRearLeftDutySet, slopespeed);
+	set_duty_cycle(pFrontLeftDutySet, slopespeed);
+}
+
+void pos_halten() {
 	*pwm_enable = 0;
+	delay(2);
+	set_duty_cycle(pFrontRightDutySet, 20);
+	set_duty_cycle(pRearRightDutySet, 20);
+	set_duty_cycle(pRearLeftDutySet, 20);
+	set_duty_cycle(pFrontLeftDutySet, 20);
+	*pwm_enable = (ALL_WHEEL_BWD_MASK);
+	delay(delayXXX*3);
+
+	set_duty_cycle(pFrontRightDutySet, slopespeed);
+	set_duty_cycle(pRearRightDutySet, slopespeed);
+	set_duty_cycle(pRearLeftDutySet, slopespeed);
+	set_duty_cycle(pFrontLeftDutySet, slopespeed);
 }
 
 void nach_links() {
+	*pLights = 0x8;
 	*pwm_enable = 0;
 	delay(2);
 	if(v > 0){
@@ -136,17 +238,18 @@ void nach_links() {
 		*pwm_enable = (REAR_RIGHT_WHEEL_FWD_MASK);
 	} else {
 		set_duty_cycle(pFrontRightDutySet, 0);
-		set_duty_cycle(pRearRightDutySet, 0);
+		set_duty_cycle(pRearRightDutySet, slopespeed * 2);
 		set_duty_cycle(pRearLeftDutySet, 0);
-		set_duty_cycle(pFrontLeftDutySet, slopespeed * 2);
-		*pwm_enable = (FRONT_LEFT_WHEEL_FWD_MASK);
+		set_duty_cycle(pFrontLeftDutySet, 0);
+		*pwm_enable = (REAR_RIGHT_WHEEL_BWD_MASK);
 	}
 	delay(2000000);
 	*pwm_enable = 0;
+	*pLights = 0x1;
 }
 
 void nach_rechts() {
-	*pwm_enable = 0;
+	*pwm_enable = 0x3;
 	delay(2);
 	if(v > 0){
 		set_duty_cycle(pFrontRightDutySet, 0);
@@ -155,13 +258,13 @@ void nach_rechts() {
 		set_duty_cycle(pFrontLeftDutySet, 0);
 		*pwm_enable = (REAR_LEFT_WHEEL_FWD_MASK);
 	} else {
-		set_duty_cycle(pFrontRightDutySet, slopespeed * 2);
+		set_duty_cycle(pFrontRightDutySet, 0);
 		set_duty_cycle(pRearRightDutySet, 0);
-		set_duty_cycle(pRearLeftDutySet, 0);
+		set_duty_cycle(pRearLeftDutySet, slopespeed * 2);
 		set_duty_cycle(pFrontLeftDutySet, 0);
-		*pwm_enable = (FRONT_RIGHT_WHEEL_FWD_MASK);
+		*pwm_enable = (REAR_LEFT_WHEEL_BWD_MASK);
 	}
 	delay(2000000);
 	*pwm_enable = 0;
+	*pLights = 0x1;
 }
-
